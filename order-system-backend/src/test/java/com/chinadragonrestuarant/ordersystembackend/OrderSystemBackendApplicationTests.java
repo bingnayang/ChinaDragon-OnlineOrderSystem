7feef814a640847323d8507d4f6ceff7eccaac2a
BingNa Yang <bingnayang@gmail.com>
2021-01-02 14:17:17 -0500

    Initial commit

---
+++ order-system-backend/src/test/java/com/chinadragonrestuarant/ordersystembackend/OrderSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinadragonrestuarant.ordersystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
