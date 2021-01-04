41f76a3ee0c4da47027db806ccb6a62de6361861
BingNa Yang <bingnayang@gmail.com>
2021-01-04 15:26:44 -0500

    update routing paths and and add router links to navigation bar

--- order-system-frontend/src/app/app-routing.module.ts
+++ order-system-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ActiveOrderComponent } from './active-order/active-order.component';
 import { HomeComponent } from './home/home.component';
+import { ViewCommentComponent } from './view-comment/view-comment.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'active-order', component: ActiveOrderComponent},
+  {path: 'view-comment', component: ViewCommentComponent},
   {path: '', redirectTo:'home', pathMatch: 'full'}
 ];
 
