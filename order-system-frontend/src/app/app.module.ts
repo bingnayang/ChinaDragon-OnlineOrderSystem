1bc4143c234753452cf80dc50eca46d6b069a512
BingNa Yang <bingnayang@gmail.com>
2021-01-04 14:58:51 -0500

    create home and active-order components, update routing paths

--- order-system-frontend/src/app/app.module.ts
+++ order-system-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActiveOrderComponent } from './active-order/active-order.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActiveOrderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
