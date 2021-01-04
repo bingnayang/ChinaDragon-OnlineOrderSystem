44101e767bf48621ef2522fbf8a271ad921724ba
BingNa Yang <bingnayang@gmail.com>
2021-01-04 18:12:16 -0500

    create active-order-detail component

---
+++ order-system-frontend/src/app/active-order/active-order-detail/active-order-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-order-detail',
+  templateUrl: './active-order-detail.component.html',
+  styleUrls: ['./active-order-detail.component.css']
+})
+export class ActiveOrderDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
